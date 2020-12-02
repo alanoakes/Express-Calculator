64ef42478311e6dee20260193ece4a6ee5cbca60
Parker Oakes <parker.oakes@clkmm.com>
2020-12-02 01:02:02 -0600

    added bodyParser to js

--- calculator.js
+++ calculator.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
 });
 
 app.post('/', (req, res) => {
-	res.send('Thanks for posting that!')
+	console.log(req.body); // bodyParser lets you grab post data
+	res.send('Thanks for posting that!');
 });
 
 app.listen(port, () => {
